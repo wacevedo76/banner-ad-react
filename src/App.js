3b8c92e7248adb5d1c25fb1c941cf2d073c9cfc5
William Acevedo <william.acevedo@gmail.com>
2019-10-09 14:20:43 +0200

    Added two googole fonts to index.css and moved .container.  added .container and <Ad/> references to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import Ad from './advert/ad_main.jsx';
 function App() {
   return (
-    <div className="App">
-    
+    <div className="container">
+      <Ad/>
     </div>
   );
 }
