9e8e240b1feffdf6afc6c789f679419fb8af07a7
William Acevedo <william.acevedo@gmail.com>
2019-10-09 22:30:02 +0000

    created bottom partials for ad bottom

---
+++ src/advert/ad_b_bottom.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdBBottom = () => {
+  return (
+    <div className="ad-b-bottom">
+    this is where the time goes
+    </div>
+  );
+};
+
+export default AdBBottom;
---
+++ src/advert/ad_b_top.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdBtop = () => {
+  return (
+    <div className="ad-b-top">
+    this is where the time goes
+    </div>
+  );
+};
+
+export default AdBtop;
