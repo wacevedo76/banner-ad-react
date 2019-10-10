54191d32f7201018d48df5c4558bb1f79b6157d8
William Acevedo <william.acevedo@gmail.com>
2019-10-10 02:07:37 +0200

    started countdown function (did not add Javascript yet)

--- src/advert/ad_b_bottom.jsx
+++ src/advert/ad_b_bottom.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const AdBBottom = () => {
   return (
     <div className="ad-b-bottom">
-    this is where the time goes
+      <div className="ad-countdown-timer countdown-border">18</div>
+      <div className="ad-countdown-timer">:</div>
+      <div className="ad-countdown-timer countdown-border">10</div>
+      <div className="ad-countdown-timer">:</div>
+      <div className="ad-countdown-timer countdown-border">23</div>
     </div>
   );
 };
